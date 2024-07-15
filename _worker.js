0741fd6817925cfa17d1d1dc3f5f52d10a5f3cb7
wuchenivy2006 <154883355+wuchenivy2006@users.noreply.github.com>
2024-07-15 11:34:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'c1dea4f4-b595-4bda-b7ce-2ba0327b68d0';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
